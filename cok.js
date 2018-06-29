b23bf3abba6df68686e164ced75742a1b6b14bcd
mood321 <he12345678@foxmail.com>
2018-06-29 18:49:55 +0800

    第一次

--- cok.js
+++ cok.js
@@ -1,6 +1,9 @@
 const superagent=require('superagent');
 
 exports.getCookie=function(success){
+	
+	
+	//1
     superagent.post('http://fulinono.com/wp-login.php')
         .type('form')
         .send({
